0c4a66e71f33eae94b4fbb701c611629f2befcde
shareeb hashmi <shareeb.hashmi@student.howest.be>
2023-03-16 21:50:08 +0100

    feat: project-setup and added tailwindcss and reactDOM

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div
+        className="text-4xl text-emerald-500 bg-white dark:bg-gray-800 dark:text-white"
+      >
+        Hello world!
+      </div>
+    ),
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
