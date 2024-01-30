62fe5af00a2a481f87bbba1ec565e3bf9512808a
shareeb hashmi <shareeb.hashmi@student.howest.be>
2024-01-30 20:35:09 +0100

    commit

--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -17,55 +17,10 @@ export default () => {
         <h3 className="text-4xl py-2 font-semibold bg-gradient-to-r bg-clip-text text-transparent from-teal-300 to-blue-500">
           My Projects
         </h3>
-        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+        {/* <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
           Some of my projects that I have worked on. Some of them are still in
           progress!
-        </p>
-        <span className="flex flex-col gap-10 py-10 lg:flex-row items-center rounded-md hover:text-teal-500 duration-500 transition-colors">
-          <div className="basis-1/3 flex-1">
-            <img src={movieApp} className="object-cover rounded-md" alt="" />
-          </div>
-          <div className="basis-1/3 flex-1 ">
-            <h1 className="capitalize  text-lg font-semibold">The movie app</h1>
-            <p className="text-sm  font-normal text-gray-800 dark:text-white leading-6 ">
-              An app that shows the latest movies with movie ratings and a
-              searchbar to search for your favorite movies. This app is made
-              with the{" "}
-              <a
-                href="https://www.themoviedb.org/"
-                target="_blank"
-                className="text-teal-500"
-              >
-                The Movie Database (TMDb) API
-              </a>
-              . there is a lightmode option aswell but it is based on the
-              settings of your device/sytem which the darkmode or lightmode is
-              applied.
-            </p>
-            <div className="flex gap-4 mt-5">
-              <a
-                href="https://hashmishareeb.github.io/movieapp/"
-                target="_blank"
-                className="hover:cursor-pointer"
-              >
-                <ExternalLink />
-              </a>
-              <a
-                href="https://github.com/HashmiShareeb/shareebhashmi.github.io"
-                target="_blank"
-                className="hover:cursor-pointer"
-              >
-                <GithubIcon />
-              </a>
-            </div>
-            <div className="flex mt-5 gap-4 dark:text-white">
-              <h5 className="text-sm font-light">Javascript</h5>
-              <h5 className="text-sm font-light">HTML</h5>
-              <h5 className="text-sm font-light">CSS</h5>
-            </div>
-          </div>
-        </span>
-
+        </p> */}
         <span className="flex flex-col gap-10 py-10 lg:flex-row-reverse items-center rounded-md hover:text-teal-500 duration-500 transition-colors">
           <div className="basis-1/3 flex-1">
             <img
@@ -111,6 +66,51 @@ export default () => {
             </button> */}
           </div>
         </span>
+        <span className="flex flex-col gap-10 py-10 lg:flex-row items-center rounded-md hover:text-teal-500 duration-500 transition-colors">
+          <div className="basis-1/3 flex-1">
+            <img src={movieApp} className="object-cover rounded-md" alt="" />
+          </div>
+          <div className="basis-1/3 flex-1 ">
+            <h1 className="capitalize  text-lg font-semibold">The movie app</h1>
+            <p className="text-sm  font-normal text-gray-800 dark:text-white leading-6 ">
+              An app that shows the latest movies with movie ratings and a
+              searchbar to search for your favorite movies. This app is made
+              with the{" "}
+              <a
+                href="https://www.themoviedb.org/"
+                target="_blank"
+                className="text-teal-500"
+              >
+                The Movie Database (TMDb) API
+              </a>
+              . there is a lightmode option aswell but it is based on the
+              settings of your device/sytem which the darkmode or lightmode is
+              applied.
+            </p>
+            <div className="flex gap-4 mt-5">
+              <a
+                href="https://hashmishareeb.github.io/movieapp/"
+                target="_blank"
+                className="hover:cursor-pointer"
+              >
+                <ExternalLink />
+              </a>
+              <a
+                href="https://github.com/HashmiShareeb/shareebhashmi.github.io"
+                target="_blank"
+                className="hover:cursor-pointer"
+              >
+                <GithubIcon />
+              </a>
+            </div>
+            <div className="flex mt-5 gap-4 dark:text-white">
+              <h5 className="text-sm font-light">Javascript</h5>
+              <h5 className="text-sm font-light">HTML</h5>
+              <h5 className="text-sm font-light">CSS</h5>
+            </div>
+          </div>
+        </span>
+
         <h3 className="text-2xl font-semibold text-teal-500">
           Some other projects
         </h3>
@@ -124,10 +124,7 @@ export default () => {
             <p className="text-sm leading-6">
               I recently created a quote app using Xamarin Forms and my own API
               with Azure Functions. The app provides users with an extensive
-              collection of famous quotes from various sources. The app's clean
-              and intuitive interface makes it easy to navigate and search for
-              quotes, and the API backend ensures a seamless and fast user
-              experience. Thanks to Azure Functions.
+              collection of famous quotes from various sources.
               <div className="flex mt-4 gap-4"></div>
             </p>
           </div>
